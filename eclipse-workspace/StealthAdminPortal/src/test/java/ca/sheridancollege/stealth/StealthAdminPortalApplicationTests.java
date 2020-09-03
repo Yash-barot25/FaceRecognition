298a826946ad711a8b6ebba69ab4a66c287837cd
bhavi <bhavi@192.168.0.185>
2020-09-03 13:24:15 -0400

    Initial Commit

---
+++ eclipse-workspace/StealthAdminPortal/src/test/java/ca/sheridancollege/stealth/StealthAdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.stealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StealthAdminPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
